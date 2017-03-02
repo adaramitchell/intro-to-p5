5fdb452ecfa1f3fbe1dbe4097bde030b56e3dae6
Adara Mitchell <amitchell@wlapcs.org>
2017-03-02 18:21:04 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,53 @@
+function setup(){
+    createCanvas(600,400);
+    background(69, 128, 165);
+}
+
+function draw(){
+  line(35, 80, 400, 200);
+  
+   stroke(255,255,255);
+  fill(255,0,0);
+  rect(525,100,50,250); 
+  
+ stroke(255,255,255);
+  fill(255,0,0);
+  rect(525,100,50,250);  
+ 
+  
+  stroke(255,255,255);
+  fill(255,0,0);
+  rect(290,100,200,250);  
+  
+  stroke(0,0,0);
+  strokeWeight(4);
+  fill(0,0,255)
+  ellipse(100,45,75,80);
+  
+  stroke(0,0,0);
+  strokeWeight(4);
+  fill(0,0,255)
+  ellipse(50,260,80,80);
+ 
+ stroke(0,0,0);
+  strokeWeight(4);
+  fill(0,0,255)
+  ellipse(375,150,75,80);
+ 
+
+ stroke(0,0,0);
+  strokeWeight(4);
+  fill(0,0,255)
+  ellipse(200,50,75,80);
+  
+   stroke(0,0,0);
+  strokeWeight(4);
+  fill(0,0,255)
+  ellipse(300,50,75,80);
+  
+   line(40,90,500,300);
+   line(-9, 595, 10000, -9000);
+   line(-198, 216, 30050, 980);
+   line(389, .2, 91, 499990);
+ 
+}
